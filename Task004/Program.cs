51057690213988261ac62cd1e9908f065c6ce399
Katz Vera <Katz.VG@gmail.com>
2023-01-14 16:32:13 +0300

    add task 004 через while

---
+++ Task004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 5: На вход число N. На выход диапазон от -N до +N.
+// Через цикл while.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = n * (-1);
+while (i <= n)
+{
+    Console.Write($"{i}, ");
+    i++; //шаг на i + 1
+}
