07b68907bd88b63fa1d63847ea0ab3d97f921a6e
Katz Vera <Katz.VG@gmail.com>
2023-01-14 16:27:54 +0300

    add task 001

---
+++ Task001/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача: на вход число. На выход кватрат числа.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = x * x;
+Console.WriteLine($"квадрат числа {x} = {y}");
