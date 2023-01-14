c18318beb4cdf74eb9560c33be4cbdc92c6285a3
Katz Vera <Katz.VG@gmail.com>
2023-01-14 16:54:35 +0300

    add task 005

---
+++ Task005/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 7: На вход число N. На выход последняя цифра числа N.
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = n % 10;
+Console.WriteLine($"последняя цифра числа: {i} ");
+
