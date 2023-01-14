eb12162a7d5055983b54af7aa1baa4f4be28fad0
Katz Vera <Katz.VG@gmail.com>
2023-01-14 16:28:39 +0300

    add task 002

---
+++ Task002/Program.cs
@@ -0,0 +1,18 @@
+﻿// задача 1: На вход 2 числа. Проверка является ли 1е число квадратом 2го. если до, то "да", если нет - то "нет".
+
+Console.Clear();
+
+Console.Write("Введите число 1: ");
+int N1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int N2 = Convert.ToInt32(Console.ReadLine());
+
+if (N1 == N2 * N2)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
