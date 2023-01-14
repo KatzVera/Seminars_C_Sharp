3656b2d7130e0c9e8d588e3a7b4063ef9aadbc19
Katz Vera <Katz.VG@gmail.com>
2023-01-14 16:32:52 +0300

    add task 004 через for

---
+++ Task004_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 5: На вход число N. На выход диапазон от -N до +N.
+// Через цикл for.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for( int i = n * (-1); i <= n; i++)
+    {
+        Console.Write($"{i}, ");
+    }
