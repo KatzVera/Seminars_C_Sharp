50ba461e4dde206f0a327cf9ca929acfae285f51
Katz Vera <Katz.VG@gmail.com>
2023-01-16 22:24:08 +0300

    Задача 8

---
+++ HomeWork004/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 8: Программа на вход получается число N.
+           На выход показывает все четные числа от 1 до N.
+ Например: 5 -> 2, 4
+           8 -> 2, 4, 6, 8.
+*/
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i <= n; i +=2)
+{
+    Console.Write($"{i}, ");
+}
+
