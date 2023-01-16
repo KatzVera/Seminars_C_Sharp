5e0dc36c30461592241c2c1cf55acc933c2f6dfe
Katz Vera <Katz.VG@gmail.com>
2023-01-16 20:17:41 +0300

    Задача 6

---
+++ HomeWork003/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 6: Программа на вход принимает числа, на выход выдает число четное или нет.
+     Пример: 4 -> да; -3 -> нет; 7 -> нет.
+*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 0)
+{
+    Console.WriteLine($" {n} - четное");
+}
+else
+{
+    Console.WriteLine($" {n} - не четное");
+}
