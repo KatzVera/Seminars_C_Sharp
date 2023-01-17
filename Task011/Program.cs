1d7384fb56e6de647feb78c186f710bfe16ecfa5
Katz Vera <Katz.VG@gmail.com>
2023-01-17 23:37:51 +0300

    add task 011

---
+++ Task011/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача: Программа выводит случайное числа из отрезка [10; 99]. И выводит большую цифру этого числа.
+// Пример: 78 -> 8; 12 -> 2; 85 -> 8.
+
+Console.Clear();
+int n = new Random().Next(10, 100);
+Console.WriteLine(n);
+
+if (n % 10 > n / 10)
+{
+    Console.WriteLine(n % 10);
+}
+else
+{
+    Console.WriteLine(n / 10);
+}
+
