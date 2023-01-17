ee4399eddaef9414b142c8eaeac7250256857fc0
Katz Vera <Katz.VG@gmail.com>
2023-01-17 23:35:47 +0300

    add task 009

---
+++ Task009/Program.cs
@@ -0,0 +1,21 @@
+﻿// Цикл с условтем. Прекращение цикла с условием.
+
+Console.Clear();
+int a = 5;
+
+
+while (a < 10)
+{
+    
+    a++; 
+    Console.WriteLine(a);
+    
+    if (a == 8)
+    {
+       Console.WriteLine("111");
+       break; 
+    }
+    
+}
+Console.WriteLine("OK");
+
