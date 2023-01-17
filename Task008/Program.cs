547485042f142ca5a337848834fd240cc5e89ba1
Katz Vera <Katz.VG@gmail.com>
2023-01-17 23:35:21 +0300

    add task 008

---
+++ Task008/Program.cs
@@ -0,0 +1,14 @@
+﻿// Условный цикл.
+
+Console.Clear();
+int a = 5;
+
+
+while (a < 10) // пока условие истина, код будет выполняться
+{
+    Console.WriteLine(a); 
+    a++; // если цикл бесконечный, прервать его можно ctrl + C
+    break;
+}
+Console.WriteLine("OK");
+
