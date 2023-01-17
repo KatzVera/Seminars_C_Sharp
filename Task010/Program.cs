e4151280da3174cc40f417aaa4aa4e910749a7de
Katz Vera <Katz.VG@gmail.com>
2023-01-17 23:36:31 +0300

    add task 010

---
+++ Task010/Program.cs
@@ -0,0 +1,17 @@
+﻿// Счетный цикл.
+
+Console.Clear();
+int a = 5;
+
+for (int i =0; i < 10; i++)
+{
+    if(i == 8 || i == 7)
+    {
+        continue;
+        Console.WriteLine("111");
+    }
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("OK");
+
