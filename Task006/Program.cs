3dc5f1ef73ca7176d4608176c3e960692c8b0878
Katz Vera <Katz.VG@gmail.com>
2023-01-17 23:34:14 +0300

    add task 006

---
+++ Task006/Program.cs
@@ -0,0 +1,21 @@
+﻿// Каскадное условие.
+
+Console.Clear();
+int a = 15;
+
+if (a < 10)
+{
+    Console.WriteLine(1); // когда программа нашла выполнение условия, она прекращает рассматривать дальше
+}
+else if (a < 20)
+{
+    Console.WriteLine(2);
+}
+else if (a < 30)
+{
+    Console.WriteLine(3); // при a=15 это условие уже не проверяется
+}
+else
+{
+    Console.WriteLine(4);
+}
