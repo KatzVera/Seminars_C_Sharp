464ce60d9b85360ca84f50c0427d46bb062a5775
Katz Vera <Katz.VG@gmail.com>
2023-01-24 19:40:45 +0300

    Задача 23

---
+++ HomeWork010/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23: Программа на вход принимает число N.
+// На выход выдает таблицу кубов числе от 1 до N.
+// Пример: 3 -> 1, 8, 27
+//         5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+if (n <= 0)
+{
+    Console.WriteLine("Неверное число, введите положительное число");
+}
+else if (n > 0)
+{
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine(Math.Pow(i + 1, 3));
+    }
+}
