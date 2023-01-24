d79252cce1c41d9e5ed9f3b860ff56e7917b5980
Katz Vera <Katz.VG@gmail.com>
2023-01-24 19:38:32 +0300

    Задача 19

---
+++ HomeWork008/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 19: Программа на ввод принимает пятизначное число, и проверяет является ли оно полиндромом.
+// Пример : 14212 -> no; 12821 -> yes; 23432 -> yes.
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = n / 10000;
+int b = n % 10000/ 1000;
+int c = n % 100 / 10;
+int d = n % 10;
+
+if (n < 10000 || n > 99999)
+{
+    Console.WriteLine("число не пятизначное");
+}
+else if (a == d && b == c)
+{
+    Console.WriteLine($"число {n} - полиндром");
+}
+else
+{
+    Console.WriteLine($"число {n} - не полиндром");
+} 
