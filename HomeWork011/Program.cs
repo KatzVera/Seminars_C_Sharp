827dd136735600539b5ca0dadf92da28ea6ff8d4
Katz Vera <Katz.VG@gmail.com>
2023-01-28 17:40:53 +0300

    Задача 25

---
+++ HomeWork011/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 25: Программа принимает на вход числа A и B. На выход возводит A в натуральную степень B (циклом).
+// пример: 3, 5 -> 243; 2, 4 -> 16
+
+Console.Clear();
+Console.WriteLine("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = a;
+
+for (int i = 0; i < b - 1; i++)
+{
+    c = c * a;
+}
+
+Console.WriteLine($"Число {a} в степени {b} = {c}");
