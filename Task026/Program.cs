21ad764ef0f82d0f458e7cd5945f2e95a09086f2
Katz Vera <Katz.VG@gmail.com>
2023-01-28 17:11:01 +0300

    add task 26

---
+++ Task026/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 31:
+
+int[] array = new int[12];
+int sumPos = 0;
+int sumNeg = 0;
+
+Console.WriteLine($"Длинна массива = {array.Length}");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]= new Random().Next(-9, 10);
+    Console.Write($" {array[i]}");
+
+    if (array[i] > 0)
+    {
+        sumPos = sumPos + array[i];
+    }
+    else if (array[i] < 0)
+    {
+        sumNeg = sumNeg + array[i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine ($"сумма положительных элементов {sumPos}");
+Console.WriteLine ($"сумма отрицательных элементов {sumNeg}");
+
+
