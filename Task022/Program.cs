5f9da7706b0a1b4f087f8c4e04d6149b5713b7f2
Katz Vera <Katz.VG@gmail.com>
2023-01-28 17:09:26 +0300

    add task 22

---
+++ Task022/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 26: Программа на вход принимает число. На выход выдает количество цифр в числе.
+// Пример: 456 -> 3; 78 -> 2; 89126 -> 5.
+
+
+int Count (int n)
+{
+
+int i = 0;
+
+while (n != 0)
+    {
+        n = n / 10;
+        i++;
+    }
+    return i;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"количество цифр {Count(n)}");
+
+
+
