adf0053fbd4a8892aef016addf12791392182bae
Katz Vera <Katz.VG@gmail.com>
2023-01-28 17:10:14 +0300

    add task 24

---
+++ Task024/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 30: Программа выводит масси из 8 элементов, заполняемый 0 и 1 в случайном порядке.
+// Пример: [1, 0, 1, 1, 0, 1, 0, 0].
+
+int[] array = new int[8];
+Console.WriteLine($"Длинна массива = {array.Length}");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]= new Random().Next(0, 2);
+    Console.Write($" {array[i]}");
+}
+
