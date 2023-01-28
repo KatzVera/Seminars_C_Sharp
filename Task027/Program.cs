1c252321de0c8d917134a68f1f8487608db233dc
Katz Vera <Katz.VG@gmail.com>
2023-01-28 17:11:29 +0300

    add task 27

---
+++ Task027/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 32:
+
+int[] array = new int [5];
+int[] newArray = new int [5];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]= new Random().Next(-9, 10);
+    Console.Write($" {array[i]}");
+}
+Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    newArray[i] = array[i] * -1;
+    Console.Write($" {newArray[i]}");
+}
+
