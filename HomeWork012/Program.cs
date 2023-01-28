94e6b3130d6b2a84ebb1c44c37f3cd464d1bac89
Katz Vera <Katz.VG@gmail.com>
2023-01-28 17:41:22 +0300

    Задача 27

---
+++ HomeWork012/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Программа на вход принимает число. На выход выдает сумму цифр числа.
+// Пример: 452 -> 11; 82 -> 10; 9012 -> 12.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"количество цифр {Count(n)}");
+int a = 0;
+int b = n % 10;
+
+while (n != 0)
+{
+    n = n / 10;
+    a = n % 10;
+    b = b + a; 
+}
+
+Console.WriteLine($"сумма цифр числа = {b}");
