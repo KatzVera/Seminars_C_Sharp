fdabcaa67d6b4a8f6086f286a2a896c4c067d880
Katz Vera <Katz.VG@gmail.com>
2023-01-28 17:41:47 +0300

    Задача 29

---
+++ HomeWork013/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 29: Программа которая задает массив из 8 элементов и выводит их на экран.
+// Пример: 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]; 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+int[] array = new int[8];
+
+Console.WriteLine($"Длинна массива = {array.Length}");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]= new Random().Next(-20, 20);
+    Console.Write($" {array[i]}");
+}
