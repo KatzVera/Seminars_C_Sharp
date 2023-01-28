7c22d3b7decf945f0f0b84039ee35f4894c7e762
Katz Vera <Katz.VG@gmail.com>
2023-01-28 17:09:48 +0300

    add task 23

---
+++ Task023/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 28: Программа на вход принимает число N. На выход выдает произведение всех чисел от 1 до N.
+// Пример: 4 -> 24; 5 -> 120.
+
+int Factorial (int n) // метод принимающий аргумент и возвращающий значение
+{
+int res = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        res = res * i;
+    }
+    return res;
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{n} -> {Factorial(n)}");
