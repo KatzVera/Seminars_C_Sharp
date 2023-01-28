acd92aa6699ee03d852b9b6ff75fcbb2a591f8f4
Katz Vera <Katz.VG@gmail.com>
2023-01-28 23:00:54 +0300

    Задача 36

---
+++ HomeWork015/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 36: Программа задает массив из случайных чисел.
+// На выход считает сумму чисел стоящих на нечетных позициях.
+
+Console.Clear();
+int[] array = new int[6];
+int sum = 0;
+
+Console.WriteLine("Массив: ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]= new Random().Next(-99, 100);
+    Console.Write($"{array[i]} ");
+}
+
+for (int i = 1; i < array.Length; i = i + 2)
+{
+    sum = sum + array[i];
+}
+
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел нечетных элементов = {sum}");
