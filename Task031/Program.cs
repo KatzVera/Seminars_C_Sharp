70ff181ee0aae49b2c53504f7253cdd3554fa57e
Katz Vera <Katz.VG@gmail.com>
2023-01-31 18:00:41 +0300

    add task 31

---
+++ Task031/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 40: Программа принимает на вход 3 числа и проверяет, 
+//            может ли существовать треугольник с такими сторонами.
+// Теорема о неравенства треугоника: каждая сторона треугольника меньше суммы двух других сторон.
+
+Console.Clear();
+Console.WriteLine("Введите 1-е число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-е число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3-е число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a < b + c && b < a + c && c < a + b)
+{
+    Console.WriteLine($"треугольник со сторонами {a}, {b}, {c} может существовать");
+}
+else
+{
+    Console.WriteLine("Такой треугольник не может существовать");
+}
