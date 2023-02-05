9ce0f8c57786098a95eeadbe695ed416ec200d43
Katz Vera <Katz.VG@gmail.com>
2023-02-05 21:36:31 +0300

    add task 33 s7

---
+++ Task033/Program.cs
@@ -0,0 +1,19 @@
+﻿// Двумерные массивы
+//Задача 46: Программа задает двумерный массив размером M x N и заполняет его случайными целыми числами.
+
+Console.Clear();
+Console.WriteLine("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+ for (int i = 0; i < array.GetLength(0); i++)
+ {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(-99, 100);
+        Console.Write($" {array[i,j]}");
+    }
+    Console.WriteLine();
+ }
