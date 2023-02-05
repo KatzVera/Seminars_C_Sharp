b2fe8e01d71cb167c10fa7814cbdf9f10163c87a
Katz Vera <Katz.VG@gmail.com>
2023-02-05 21:36:52 +0300

    add task 34 s7

---
+++ Task034/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 48: Программа задает двумерный массив M x N, каждый элемент массива находится по формуле Amn = M + N.
+//            Выводим массив на экран. 
+
+Console.Clear();
+Console.WriteLine("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+ for (int i = 0; i < array.GetLength(0); i++)
+ {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = i + j;
+        Console.Write($" {array[i,j]}");
+    }
+    Console.WriteLine();
+ }
