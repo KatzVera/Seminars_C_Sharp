a9eae73c9c6ba95b031faf68cf0342bb10a5a437
Katz Vera <Katz.VG@gmail.com>
2023-02-11 18:28:13 +0300

    add task 43

---
+++ Task043/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67: Программа на вход принимает число. Возвращает сумму всех цифр числа.
+// Пример: 453 -> 12
+//         45 -> 9
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Summ (int num)
+{
+    if (num == 0)
+        return num;
+    return num % 10 + Summ(num / 10);
+    
+}
+Console.WriteLine($"Сумма цифр числа: {Summ(num)}");
+
