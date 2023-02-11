6e326b2051cc96d2e40e7f39b1f8936204f5294a
Katz Vera <Katz.VG@gmail.com>
2023-02-11 18:28:30 +0300

    add task 44

---
+++ Task044/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 69: Программа принимает на вход числа A и B. На выход возводит число A в степень B.
+// Пример: A = 3, B = 5 -> 243 (3 в степень 5)
+//         A = 2; B = 3 -> 8
+
+Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень числа А: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int Deg (int a, int b)
+{
+    if (b == 1)
+        return a;
+    return a * Deg(a, b - 1);
+    
+}
+Console.WriteLine($"Результат = {Deg(a,b)}");
