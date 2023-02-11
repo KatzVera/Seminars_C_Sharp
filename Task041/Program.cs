49879d3d87634e1392b2e41ad1e6fa1c945f21a5
Katz Vera <Katz.VG@gmail.com>
2023-02-11 18:27:17 +0300

    add task 41

---
+++ Task041/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 63: задайте значение N. Программа выдает все натуральные числа от 1 до N.
+// Пример: N = 5 -> 1 2 3 4 5;
+//         N = 6 -> 6 5 4 3 2 1;
+
+
+//Возвращаемый метод
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Nat (int num)
+{
+    Console.Write($"{num} ");
+    if (num == 1)
+        return 1;
+    return num - Nat(num - 1);
+}
+Console.WriteLine();
+Nat(num); 
+
+// Невозвращаемый метод:
+/*
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void Nat (int num)
+{
+    if (num == 1)
+    {
+        Console.Write($"{num} ");
+    }
+    else
+    {
+        Nat(num - 1);
+        Console.Write($"{num} ");
+    } 
+    
+}
+
+Nat(num);
+*/
+
+
