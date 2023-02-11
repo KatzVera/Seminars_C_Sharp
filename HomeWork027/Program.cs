012df9ea71f80d67da643e64b34f004ebff40787
Katz Vera <Katz.VG@gmail.com>
2023-02-11 18:21:54 +0300

    Задача 64

---
+++ HomeWork027/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задачате значение N. Программа выводит все натуральные числа в промежутке от N до 1.
+// Пример: N = 5 -> 5 4 3 2 1
+//         N = 8 -> 8 7 6 5 4 3 2 1
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Nat (int n)
+{
+    if (n == 1)
+    {
+        Console.Write($"{n} ");
+    }
+    else
+    {
+        Console.Write($"{n} ");
+        Nat(n - 1);
+    } 
+    
+}
+
+Nat(n);
