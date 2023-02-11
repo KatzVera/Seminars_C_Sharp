4b68ec9242ea4913ad5f51a64067b2bead48ade5
Katz Vera <Katz.VG@gmail.com>
2023-02-11 18:26:57 +0300

    add task 40

---
+++ Task040/Program.cs
@@ -0,0 +1,12 @@
+﻿// Рекурсия
+
+//Факториал числа : n! = 1*2*3*...*n
+
+int Factorial(int num) // ";" не надо!!!!!
+{
+    if(num == 1)
+        return 1;
+    return num * Factorial(num - 1);
+}
+
+Console.WriteLine(Factorial(5));
