747e417814a5948570c5ea1e92e29629f1bd0112
Katz Vera <Katz.VG@gmail.com>
2023-02-11 18:23:10 +0300

    Задача 68

---
+++ HomeWork029/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 68: Введите два числа m и n.
+//            Программа вычисляет функции Аккермана с помощью рекурсии для двух чисел.
+// Пример: m = 2 n = 3 -> A(m,n) = 29
+//         n = 3 m = 2 -> A(m,n) = 9
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int Ack(int n, int m)
+{
+    while (n != 0)
+    {
+
+        if (m == 0)
+            m = 1;
+        else
+            m = Ack(n, m - 1);
+
+        n--;
+    }
+    return m + 1;
+
+}
+Console.WriteLine($"Результат = {Ack(n, m)}");
