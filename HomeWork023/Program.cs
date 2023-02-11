e831df0b0f6f66724c3a7b0985d8200ec0315690
Katz Vera <Katz.VG@gmail.com>
2023-02-11 20:10:01 +0300

    Задача 56

---
+++ HomeWork023/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 56: Задать прямоугольный двумерный массив. 
+//Программа находит и выводит строку с наименьшей суммой элементов.
+// Пример: 1 4 7 2
+//         5 9 2 3
+//         8 4 2 4
+//         Строка с наименьшей суммай элементов - 1.
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] array = new int[m,n];
+
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(0, 10);
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(array, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+
+Console.WriteLine($"Строка {minSumLine+1} - с наименьшей суммой элементов равной {sumLine}");
+
